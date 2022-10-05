3fb215157d7f7dc22477948bf331afc7f00d2ac1
kikiki10004 <kikiki10004@gmail.com>
2022-10-05 11:22:49 +1100

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+var incrementer= 0;
+var stopValue = 10;
+
+//repetition 반복 할거임 코더의 책임은 매인테인
+
+
+while (incrementer <= stopValue) {
+    System.Console.WriteLine(incrementer);
+    incrementer++;
+}
